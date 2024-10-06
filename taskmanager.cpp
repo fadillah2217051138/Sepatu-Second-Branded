d9f08d08ab195ab4b51a9a3d7b0fd39d6bdcad1a
fadillah <fadillahd544@gmail.com>
2024-10-06 21:34:47 +0700

    menambahkan variabel

--- taskmanager.cpp
+++ taskmanager.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <cmath> 
 using namespace std;
 
-int main(){
+// Fungsi untuk menampilkan menu
+void displayMenu() {
+    cout << "\n--- Menu Pilihan ---" << endl;
+    cout << "1. Konversi Suhu (Celcius ke Fahrenheit)" << endl;
+    cout << "2. Hitung Luas Lingkaran" << endl;
+    cout << "3. Hitung Faktorial" << endl;
+    cout << "4. Hitung Bilangan Fibonacci" << endl;
+    cout << "5. Keluar" << endl;
 }
