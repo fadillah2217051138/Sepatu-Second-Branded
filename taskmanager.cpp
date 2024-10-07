f22dc53c29c308aa46289eb9467ae4672043fb45
Yoesefall <84967496+YusufAlif@users.noreply.github.com>
2024-10-07 14:33:35 +0700

    Update taskmanager.cpp

--- taskmanager.cpp
+++ taskmanager.cpp
@@ -19,3 +19,13 @@ void displayShoes(Shoe, Shoes[],int size){
     }
     cout << "0. Selesai dan Hitung Total" << endl;    
 }
+
+void displayReceipt(Shoe shoes[], int itemIndex[], int quantity[], int itemCount) {
+    int subtotal = 0;
+    const double TAX_RATE = 0.10;  // Pajak 10%
+    int discount = 0;
+
+    cout << "\n--- Struk Pembelian ---" << endl;
+    cout << setw(20) << left << "Nama Sepatu" << setw(10) << left << "Jumlah" 
+         << setw(10) << left << "Harga" << setw(10) << left << "Total" << endl;
+
