60d2febca32aec0420ad6b2b81315ec98d8da76d
zahraayesha <zahraayesha0906@gmail.com>
2024-10-07 14:42:10 +0700

    Update taskmanager.cpp

--- taskmanager.cpp
+++ taskmanager.cpp
@@ -28,4 +28,42 @@ void displayReceipt(Shoe shoes[], int itemIndex[], int quantity[], int itemCount
     cout << "\n--- Struk Pembelian ---" << endl;
     cout << setw(20) << left << "Nama Sepatu" << setw(10) << left << "Jumlah" 
          << setw(10) << left << "Harga" << setw(10) << left << "Total" << endl;
+for (int i = 0; i < itemCount; i++) {
+        int index = itemIndex[i];
+        int totalPrice = shoes[index].price * quantity[i];
+        subtotal += totalPrice;
+
+        cout << setw(20) << left << shoes[index].name 
+             << setw(10) << left << quantity[i] 
+             << "Rp " << setw(10) << left << shoes[index].price 
+             << "Rp " << totalPrice << endl;
+    }
+
+    // Diskon 5% jika subtotal lebih dari Rp 2.000.000
+    if (subtotal > 2000000) {
+        discount = 0.05 * subtotal;
+        cout << "\nDiskon 5%: Rp " << discount << endl;
+    }
+
+    // Menghitung total dengan pajak dan diskon
+    int totalAfterDiscount = subtotal - discount;
+    int tax = TAX_RATE * totalAfterDiscount;
+    int total = totalAfterDiscount + tax;
+
+    cout << "\nSubtotal: Rp " << subtotal << endl;
+    cout << "Pajak (10%): Rp " << tax << endl;
+    cout << "Total Setelah Pajak: Rp " << total << endl;
+
+    int bayar;
+    cout << "Masukkan jumlah uang yang dibayarkan: Rp ";
+    cin >> bayar;
+
+    if (bayar >= total) {
+        int kembalian = bayar - total;
+        cout << "Kembalian: Rp " << kembalian << endl;
+    } else {
+        cout << "Uang tidak cukup!" << endl;
+    }
+}
+
 
